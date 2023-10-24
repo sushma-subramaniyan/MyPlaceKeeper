f2d47e57db6fdc36757ca90941ec1581b164a58e
Sanjana <sanjana.js02@gmail.com>
2023-10-24 19:45:04 +0530

    working on popup

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import Map, { Marker, Popup } from 'react-map-gl';
 import * as React from 'react';
 import "./app.css";
 import Pin from './Pin.jsx'
+import { Rating } from '@mui/material';
 
 
 
 
 function App() {
+  const [value, setValue] = React.useState(2);
   const [viewState, setViewState] = React.useState({
     longitude: 17,
     latitude: 46,
@@ -34,6 +36,35 @@ function App() {
           anchor="bottom">
           <Pin />
         </Marker>
+
+        {showPopup && (
+      <Popup 
+      longitude={2.349014} 
+      latitude={48.864716}
+        anchor="bottom"
+        closeButton={true}
+        closeOnClick={false}      
+        >
+        <div className='card'>
+          <label>Place</label>
+          <h4 className='place'>EffileTower</h4>
+          <label>Review</label>
+          <p>Beatufil place</p>
+          <label>Rating</label>
+               
+          <Rating
+          name="simple-controlled"
+          value={value}
+          onChange={(event, newValue) => {
+            setValue(newValue);
+          }} 
+          />
+          <label>Image</label>
+          <label>Information</label>
+          
+
+        </div>
+      </Popup>)}
       </Map>
     </div>
 
