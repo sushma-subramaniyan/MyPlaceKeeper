fc79dc0ca8b68fa0acc475772f7af819d4a6267c
sushma subramaniyan <sushmasubramaniyan14@gmail.com>
2023-10-24 18:26:47 +0200

    register & login in process

---
+++ src/components/Login.jsx
@@ -0,0 +1,66 @@
+import "../components/login.css"
+import "./Pin"
+import { useState } from "react";
+
+
+const Login = () => {
+    
+    const [error, setError] = useState(false);
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+   
+    const handleSubmit =async (e)=>{
+        e.preventDefault();
+        const user = {
+            username,
+            password,
+        };
+        try {
+          console.log(newUser)
+          const response = await fetch("http://localhost:5005/api/auth/signup",
+          {
+            method: "POST",
+            body: JSON.stringify(user),
+            headers:{
+                'Content-type':'application/json',
+            },
+          }
+          );
+          if(response.ok){
+            setError(false);
+          }
+           
+        } catch (error) {
+
+           setError(true); 
+        }
+    };
+    
+    return (
+     <div className="loginContainer">
+        <div className="logo">
+        { /*/have to giv pin */}
+            LamaPin
+        </div>
+        <form onSubmit={handleSubmit}>
+            <label>
+            <input value={username} placeholder="usernmae" onChange={event=>setUsername(event.target.value)} required/>
+            </label>
+            <input value={password} type="password" placeholder="password" onChange={event=>setPassword(event.target.value)} required/>
+            <button className="loginBtn">Login</button>
+        
+             { error && 
+             <span className="failure">
+                Something Went Wrong!!!!
+            </span>}
+           
+            
+        </form>
+        {/*<Cancel className="loginCancel" onClick={()=>setShowLogin(false)}/>
+        
+        we have to import cancel icon*/}
+     </div>
+    )
+  }
+  
+  export default Login
---
+++ src/components/Register.jsx
@@ -0,0 +1,75 @@
+import "../components/register.css"
+import { useState } from "react";
+import CancelIcon from '@mui/icons-material/Cancel';
+
+
+const Register = () => {
+    const [success, setSuccess] = useState(false)
+    const [error, setError] = useState(false);
+    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+   
+    const handleSubmit =async (e)=>{
+        e.preventDefault();
+        const newUser = {
+            username,
+            email,
+            password,
+        };
+        try {
+          console.log(newUser)
+          const response = await fetch("http://localhost:5005/api/auth/signup",
+          {
+            method: "POST",
+            body: JSON.stringify(newUser),
+            headers:{
+                'Content-type':'application/json',
+            },
+          }
+          );
+          if(response.ok){
+            setError(false);
+           setSuccess(true);
+          }else{
+            setError(true);
+          }
+           
+        } catch (error) {
+
+           setError(true); 
+        }
+    };
+    
+    return (
+     <div className="registerContainer">
+        <div className="logo">
+        { /*/have to giv pin */}
+            LamaPin
+        </div>
+        <form onSubmit={handleSubmit}>
+            <label>
+            <input value={username} placeholder="usernmae" onChange={event=>setUsername(event.target.value)} required/>
+            </label>
+            <input value={email} placeholder="email" onChange={event=>setEmail(event.target.value)} required/> 
+            <input value={password} type="password" placeholder="password" onChange={event=>setPassword(event.target.value)} required/>
+            <button className="registerBtn">Register</button>
+            {success && (
+            <span className="success">
+                Successfull.You can Login Now!
+            </span>
+             )}
+             { error && 
+             <span className="failure">
+                Something Went Wrong!!!!
+            </span>}
+           
+            
+        </form>
+        <CancelIcon/>       
+        
+     </div>
+    )
+  }
+  
+  export default Register
