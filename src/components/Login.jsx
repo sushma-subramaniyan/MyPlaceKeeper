6a6c62f2c2b6094d010d424428f2f27c4e1e57d3
Eugen Freund <Eugen.Freund@outlook.de>
2023-10-30 11:15:20 +0100

    add backdrop to login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -4,79 +4,90 @@ import { AuthContext } from "../contexts/AuthContext";
 import { useContext } from "react";
 import CloseIcon from '@mui/icons-material/Close';
 import RoomIcon from '@mui/icons-material/Room';
+import { Backdrop, CircularProgress } from "@mui/material";
 
 
-const Login = ({setShowLogin}) => {
-    
-    const [error, setError] = useState(false);
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
+const Login = ({ setShowLogin }) => {
 
-    const { login } = useContext(AuthContext)
-   
-    const handleSubmit =async (e)=>{
-        e.preventDefault();
-        const newUser = {
-            username,
-            password,
-        };
-        try {
-          const response = await fetch(`${import.meta.env.VITE_API_URL}/api/auth/login`,
-          {
-            method: "POST",
-            body: JSON.stringify(newUser),
-            headers:{
-                'Content-type':'application/json',
-            },
-          }
-          );
-          if (response.status === 400) {
-            const parsed = await response.json()
-            throw new Error(parsed.message)
-          }
-          if (response.status === 200) {
-            const parsed = await response.json()
-            login(parsed.token)
-            setError(false);
-            setShowLogin(false)
-          }
-           
-        } catch (error) {
-          console.log(error )
-           setError(true); 
-        }
+  const [error, setError] = useState(false);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [openBackdrop, setOpenBackdrop] = useState(false);
+
+
+  const { login } = useContext(AuthContext)
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setOpenBackdrop(true)
+    const newUser = {
+      username,
+      password,
     };
-    const handleClose =()=>{
-      setShowLogin(false)
+    try {
+      const response = await fetch(`${import.meta.env.VITE_API_URL}/api/auth/login`,
+        {
+          method: "POST",
+          body: JSON.stringify(newUser),
+          headers: {
+            'Content-type': 'application/json',
+          },
+        }
+      );
+      if (response.status === 400) {
+        const parsed = await response.json()
+        throw new Error(parsed.message)
+      }
+      if (response.status === 200) {
+        const parsed = await response.json()
+        login(parsed.token)
+        setError(false);
+        setOpenBackdrop(false)
+        setShowLogin(false)
+      }
+
+    } catch (error) {
+      console.log(error)
+      setOpenBackdrop(false)
+      setError(true);
     }
-    
-    return (
-      
-     <div className="loginContainer">
-        <div className="logo">
-        <RoomIcon />
-            MyPlaceKeeper
-        </div>
-        <form onSubmit={handleSubmit}>
-            <label>
-            <input style={{height:'18.5px',width:'240px'}} value={username} placeholder="username" onChange={event=>setUsername(event.target.value)} required/>
-            </label>
-            <input value={password} type="password" placeholder="password" onChange={event=>setPassword(event.target.value)} required/>
-            <button className="loginBtn">Login</button>
-        
-             { error && 
-             <span className="failure">
-                Something Went Wrong!!!!
-            </span>}
-           
-            
-        </form>
-        <CloseIcon className="loginCancel"  onClick={handleClose}/>
-        
-       
-     </div>
-     
-    )
+  };
+  const handleClose = () => {
+    setShowLogin(false)
   }
-  
-  export default Login
+
+  return (
+
+    <div className="loginContainer">
+      <div className="logo">
+        <RoomIcon />
+        MyPlaceKeeper
+      </div>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <input style={{ height: '18.5px', width: '240px' }} value={username} placeholder="username" onChange={event => setUsername(event.target.value)} required />
+        </label>
+        <input value={password} type="password" placeholder="password" onChange={event => setPassword(event.target.value)} required />
+        <button className="loginBtn">Login</button>
+
+        {error &&
+          <span className="failure">
+            Something Went Wrong!!!!
+          </span>}
+
+
+      </form>
+      <CloseIcon className="loginCancel" onClick={handleClose} />
+      <Backdrop
+        sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+        open={openBackdrop}
+      >
+        <CircularProgress color="inherit" />
+      </Backdrop>
+
+    </div>
+
+  )
+}
+
+export default Login
