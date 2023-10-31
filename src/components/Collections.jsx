c1cb03575e44255c948a00b6592793e4d1b1ae16
sushma subramaniyan <sushmasubramaniyan14@gmail.com>
2023-10-31 17:14:03 +0100

    collection

--- src/components/Collections.jsx
+++ src/components/Collections.jsx
@@ -6,51 +6,106 @@ import ListItemText from '@mui/material/ListItemText';
 import "../components/Collections.css"
 import { Button } from "@mui/base";
 import { TextField } from "@mui/material";
+import { useEffect } from "react";
 
 
 const Collections = () => {
 
   const [isInputVisible, setInputVisible] = useState(false);
   const [inputValue, setInputValue] = useState('');
+  const [collections, setCollections] = useState([]);
+  const [newCollectionName, setNewCollectionName] = useState('');
 
-  const items = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+ // const items = [0, 1, 2, 3, 4, 5, 6, 7, 8];
   const toggleInput = () => {
     setInputVisible(!isInputVisible);
   };
 
+
   const handleInputChange = (e) => {
     setInputValue(e.target.value);
   };
+  const handleNewCollection = async () => {
+   try {
+     const response = await fetch(`${import.meta.env.VITE_API_URL}/api/pincollection`, {
+       method: 'POST',
+       headers: {
+         'Content-Type': 'application/json',
+       },
+       body: JSON.stringify({ name: newCollectionName }),
+     });
+
+     if (response.ok) {
+       // Fetch and update the collections list after creating a new collection
+       await fetchCollections();
+     } else {
+       console.log('Failed to create a new collection');
+     }
+   } catch (error) {
+     console.log('Error:', error);
+   }
+   setInputValue('');
+   setNewCollectionName('');
+   setInputVisible(false);
+ };
+
+  useEffect(()=>{
+   const fetchCollections = async ()=>{
+      try {
+         const response = await fetch(`${import.meta.env.VITE_API_URL}/api/pincollection`);
+         if (response.ok) {
+            const data = await response.json();
+            setCollections(data);
+          } else {
+            console.log('Failed to fetch data');
+          }
+
+      } catch (error) {
+         console.log('Error:', error);
+      }
+   }
+   fetchCollections();
+  },[]);
+
 
   return (
     <div className="collections-container">
       <List dense sx={{maxHeight: '200px', overflowY: 'auto',  width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
 
-        {items.map((item, index) => {
-          const labelId = `checkbox-list-secondary-label-${item}`;
-          return (
+        {collections.map((collection) => (
+         // const labelId = `checkbox-list-secondary-label-${item}`;
+         // return (
             <ListItem
-              key={item}
+              key={collection._id}
 
               disablePadding
             >
               <ListItemButton>
-                <ListItemText id={labelId} primary={`Line item ${item + 1}`} />
+                <ListItemText  primary={collection.name} />
               </ListItemButton>
             </ListItem>
-          );
-        })}
+          ))}
        
       </List>
       <div style={{ textAlign: 'center' }}>
           {isInputVisible ? (
+            <div>
             <TextField
               variant="outlined"
               size="small"
               value={inputValue}
               onChange={handleInputChange}
               onBlur={toggleInput}
+              onKeyDown={(e) => {
+               if (e.key === 'Enter') {
+                 handleNewCollection();
+               }
+             }}
             />
+            <Button variant="contained" color="primary" onClick={handleNewCollection}>
+            Add
+          </Button>
+          </div>
           ) : (
             <Button variant="contained" color="primary" onClick={toggleInput}>
               +
